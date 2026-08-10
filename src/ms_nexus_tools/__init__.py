0b093f4eea1e74a887b46c10e45cc8e0d97d9c5e
McDougall <Duncan.McDougall@rfi.ac.uk>
2026-08-10 16:33:16 +0100

    Remove unnecesary args, plot and element code.

--- src/ms_nexus_tools/__init__.py
+++ src/ms_nexus_tools/__init__.py
@@ -4,7 +4,6 @@
 
 from . import lib as lib
 from . import api as api
-from .cli.compound import compound
 from .cli.inspect_h5 import inspect
 from .cli.query import query, bulk_query
 from .cli.nx_data_stats import print_nx_data_stats
--- src/ms_nexus_tools/api/__init__.py
+++ src/ms_nexus_tools/api/__init__.py
@@ -2,10 +2,3 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from . import image_args
-from . import image_plot
-from . import spectrum_plot
-from . import image_and_spectrum_plot
-from . import formula_args
-from . import mass_range_args
-from . import imzml
